05a17fd62d9294d0bfb47ddb07c8598722ff7e3b
Gordieiev Artem <gordieiev.artem@gmail.com>
2026-04-20 10:58:53 +0300

    20.04.2026 10:58

--- sketch_mar30a_copy_20260410165351/sketch_mar30a_copy_20260410165351.ino
+++ sketch_mar30a_copy_20260410165351/sketch_mar30a_copy_20260410165351.ino
@@ -16,10 +16,10 @@ int buzzerPin;
 char weekDay[][4] = {"Mon","Tue","Wed","Thu","Fri"};
 
 void setup() {
- Serial.begin(9600);
- clock.begin();
+  Serial.begin(9600);
+  clock.begin();
   Wire.begin();
- Serial.println("test 1");
+  Serial.println("test 1");
 }
 
 void loop() {
@@ -36,27 +36,27 @@ void loop() {
   int humidity = 0;
   int result = dht11.readTemperatureHumidity(temperature, humidity);
   if (result == 0) {
-        Serial.print("Temperature: ");
-        Serial.print(temperature);
-        Serial.print(" °C\tHumidity: ");
-        Serial.print(humidity);
-        Serial.println(" %");
-sensorf1Value = analogRead(sensorf1Pin);
-sensorf2Value = analogRead(sensorf2Pin);
-wdValue = analogRead(wdpin);
-if (wdValue <=300){
-  //tone(buzzerPin, 1000);
-  }
-else {
-  //noTone(buzzerPin);
-}
-Serial.print("Вологість землі першої квітки: ");
-Serial.println(sensorf1Value);
-Serial.print("Вологість землі другої квітки: ");
-Serial.println(sensorf2Value);
-Serial.print("Обєм води: ");
-Serial.println(wdValue);
-delay(1000);
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C\tHumidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    sensorf1Value = analogRead(sensorf1Pin);
+    sensorf2Value = analogRead(sensorf2Pin);
+    wdValue = analogRead(wdpin);
+    if (wdValue <=300){
+      //tone(buzzerPin, 1000);
+    }
+    else {
+      //noTone(buzzerPin);
+    }
+      Serial.print("Вологість землі першої квітки: ");
+      Serial.println(sensorf1Value);
+      Serial.print("Вологість землі другої квітки: ");
+      Serial.println(sensorf2Value);
+      Serial.print("Обєм води: ");
+      Serial.println(wdValue);
+      delay(1000);
   }
 
 }
