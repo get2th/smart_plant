6fdb14bb938d383eeea436906d1ee087cc9d6156
get2th <tymurpon2011@gmail.com>
2026-05-18 10:20:39 +0300

    v2.0

---
+++ sketch_mar30a/sketch_mar30a_copy_20260410165351.ino
@@ -0,0 +1,124 @@
+#include <Arduino.h>
+#include <DHT11.h>
+#include <SoftwareSerial.h>
+#include "DS3231.h"
+#include <Wire.h>
+
+DS3231 clock;
+RTCDateTime dt;
+DHT11 dht11(2);
+
+int sensorf1Pin = A0;
+int sensorf1Value = 0;
+int sensorf2Pin = A2;
+const int relay1 = 2;
+const int relay2 = 3;
+int sensorf2Value = 0;
+int wdpin = A1;
+int wdValue;
+int buzzerPin;
+int humidity;
+int result;
+char weekDay[][4] = {"Mon","Tue","Wed","Thu","Fri"};
+
+void setup() {
+  Serial.begin(9600);
+  clock.begin();
+  Wire.begin();
+<<<<<<< HEAD
+ Serial.println("test 1");
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  digitalWrite(relay1, HIGH);                
+  digitalWrite(relay2, HIGH);
+  Serial.println("test 1");
+}
+
+void loop() {
+  int temperature = 0;
+  dt = clock.getDateTime();
+  Serial.println("test 2");
+  Serial.print("Raw data: ");
+  Serial.print(dt.year);   Serial.print("-");
+  Serial.print(dt.month);  Serial.print("-");
+  Serial.print(dt.day);    Serial.print(" ");
+  Serial.print(dt.hour);   Serial.print(":");
+  Serial.print(dt.minute); Serial.print(":");
+  Serial.print(dt.second); Serial.println("");
+<<<<<<< HEAD
+  int humidity = 0;
+  int result = dht11.readTemperatureHumidity(temperature, humidity);
+  if (wdValue <=300){
+    tone(buzzerPin, 1000);
+  }
+  else {
+    noTone(buzzerPin);
+  
+    if (result == 0) {
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.print(" °C\tHumidity: ");
+      Serial.print(humidity);
+      Serial.println(" %");
+      sensorf1Value = analogRead(sensorf1Pin);
+      sensorf2Value = analogRead(sensorf2Pin);
+      wdValue = analogRead(wdpin);
+      if (dt.hour >= 17){
+        if (sensorf1Value >= 300){
+        digitalWrite(relay1, LOW);
+        }
+        else{
+
+        }
+        if (sensorf2Value >= 300){
+          digitalWrite(relay2, LOW);
+        }
+        else{
+
+        }
+      }
+      else{
+
+      }
+      }
+    else{
+
+  }
+  }
+Serial.print("Вологість землі першої квітки: ");
+Serial.println(sensorf1Value);
+Serial.print("Вологість землі другої квітки: ");
+Serial.println(sensorf2Value);
+Serial.print("Обєм води: ");
+Serial.println(wdValue);
+delay(1000);
+  
+=======
+  result = dht11.readTemperatureHumidity(temperature, humidity);
+  if (result == 0) {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C\tHumidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    sensorf1Value = analogRead(sensorf1Pin);
+    sensorf2Value = analogRead(sensorf2Pin);
+    wdValue = analogRead(wdpin);
+    if (wdValue <=300){
+      //tone(buzzerPin, 1000);
+    }
+    else {
+      //noTone(buzzerPin);
+    }
+      Serial.print("Вологість землі першої квітки: ");
+      Serial.println(sensorf1Value);
+      Serial.print("Вологість землі другої квітки: ");
+      Serial.println(sensorf2Value);
+      Serial.print("Обєм води: ");
+      Serial.println(wdValue);
+      delay(1000);
+  }
+>>>>>>> 9ef8f449cca5dbc99c62c35191acf3934554ca28
+
+}
+
